965b08f20108cb903c3c10b4cbd58bd6d2a803c4
zaherzeinni <zaherzeinni@hotmail.com>
2024-09-10 08:52:05 +0300

    okk

--- components/ClientsLogo.jsx
+++ components/ClientsLogo.jsx
@@ -56,8 +56,8 @@ useEffect(() => {
            
             {/* <span className="text-[#191919]  inline-block  uppercase font-semibold fle items-center"> */}
             
-            {/* <div  ref={ref} className={`transition-transform duration-500 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 sm:translate-y-32 -translate-y-10'}`}> */}
-            <span className=" font-raleway  uppercase sm:text-[35px] text-[27px] !font-bold ">
+            {/* <div  ref={ref} className={`font-raleway  uppercase sm:text-[35px] text-[27px] !font-bold    transition-transform duration-500 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 sm:translate-y-32 -translate-y-10'}`}> */}
+            <span  ref={ref} className="font-raleway  uppercase sm:text-[35px] text-[27px] !font-bold ">
 
            
       
